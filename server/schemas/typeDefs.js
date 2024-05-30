b36a876fbc394c69bbd93dd893985124129f87d1
meistercraft <njmeister99@gmail.com>
2024-05-30 18:45:20 -0500

    save commit

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -15,9 +15,30 @@ const typeDefs = gql`
 	username: String
   }
 
+  type GameBanner {
+	_id: ID
+	image: String
+   }
+
   type Query {
 	allUsers: [User]
 	allScores: [Score]
+  }
+
+  type Mutation {
+	addUser(username: String!, email: String!, password: String!): User
+	login(email: String!, password: String!): User
+	logout: User
+	updateUser(username: String, email: String, password: String): User
+	deleteUser(userId: ID!): User
+	addScore(score: Int!, username: String!): Score
+	deleteScore(scoreId: ID!): Score
+	addFavGame(image: String!): GameBanner
+	deleteFavGame(gameId: ID!): GameBanner
+	updateFavGame(gameId: ID!, image: String!): GameBanner
+	
+
+
   }
 `;
 
