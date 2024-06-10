c5739e983215e40d501146808d27c8a686357b66
meistercraft <njmeister99@gmail.com>
2024-06-10 12:30:40 -0500

    heroku update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/game_project');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/game_invaders');
 
 module.exports = mongoose.connection;
