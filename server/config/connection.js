6b6ba285c236cbf03f347afe9d089d6b01711729
meistercraft <njmeister99@gmail.com>
2024-06-10 13:54:25 -0500

    update for heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/game_invaders');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/game_invaders', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true,
+  sslValidate: true,
+});
 
 module.exports = mongoose.connection;
