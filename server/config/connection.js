dd45df4db4b7ff91d89fa1dc8f7123ae7380d06f
meistercraft <njmeister99@gmail.com>
2024-06-10 15:19:51 -0500

    env string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://njmeister99:26IhfrEiWRzmTDyq@cluster0.sybd48r.mongodb.net/game-invaders?retryWrites=true&w=majority&appName=Cluster0" || 'mongodb://127.0.0.1:27017/game_invaders');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/game_invaders');
 
 module.exports = mongoose.connection;
